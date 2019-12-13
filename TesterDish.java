282e8c64e9b528a1edb9c248ac77fbd24626f006
Valeriya <56223050+ValeriyZolotoskaya@users.noreply.github.com>
2019-12-13 17:10:49 +0300

    Add files via upload

---
+++ TesterDish.java
@@ -0,0 +1,8 @@
+package lab6;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("деревянная", "красная", "средняя");
+        System.out.println(t);
+    }
+}
